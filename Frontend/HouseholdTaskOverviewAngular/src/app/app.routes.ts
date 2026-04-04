28a404fd88a6f0554cd33f5d5a5507ba212801d4
Lexi22B <alex_Lulu7@yahoo.com>
2026-04-04 15:53:18 +0200

    Routing and HTTP

--- Frontend/HouseholdTaskOverviewAngular/src/app/app.routes.ts
+++ Frontend/HouseholdTaskOverviewAngular/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HouseholdList } from './component/household-list/household-list'; 
+import { HousemateList } from './component/housemate-list/housemate-list';
+import { RoomList } from './component/room-list/room-list';
+import { TaskList } from './component/task-list/task-list';
+import { TaskAssignmentList } from './component/task-assignment-list/task-assignment-list';
+import { TaskCompletionList } from './component/task-completion-list/task-completion-list';
 
-export const routes: Routes = [];
+export const routes: Routes = //routes = pages
+[
+    { path: 'households', component: HouseholdList },
+    { path: 'housemates', component: HousemateList },
+    { path: 'rooms', component: RoomList },
+    { path: 'tasks', component: TaskList },
+    { path: 'taskassignments', component: TaskAssignmentList },
+    { path: 'taskcompletions', component: TaskCompletionList },
+];
--- Frontend/HouseholdTaskOverviewAngular/src/app/app.ts
+++ Frontend/HouseholdTaskOverviewAngular/src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// app.ts = main container of the whole app, first thing which loads
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { HouseholdList } from './component/household-list/household-list';
+import { HouseholdCard } from './component/household-card/household-card';
+import { HousemateList } from './component/housemate-list/housemate-list';
+import { HousemateCard } from './component/housemate-card/housemate-card';
+import { RoomList } from './component/room-list/room-list';
+import { RoomCard } from './component/room-card/room-card';
+import { TaskList } from './component/task-list/task-list';
+import { TaskCard } from './component/task-card/task-card';
+import { TaskAssignmentList } from './component/task-assignment-list/task-assignment-list';
+import { TaskAssignmentCard } from './component/task-assignment-card/task-assignment-card';
+import { TaskCompletionList } from './component/task-completion-list/task-completion-list';
+import { TaskCompletionCard } from './component/task-completion-card/task-completion-card';
 
-@Component({
+@Component({ 
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    HouseholdList,
+    HouseholdCard,
+    HousemateList,
+    HousemateCard,
+    RoomList,
+    RoomCard,
+    TaskList,
+    TaskCard,
+    TaskAssignmentList,
+    TaskAssignmentCard,
+    TaskCompletionList,
+    TaskCompletionCard
+  ],
+  templateUrl: './app.html'
 })
-export class App {
-  protected readonly title = signal('HouseholdTaskOverviewAngular');
-}
+export class App {}
