36d34f172fa80bf02046f389f8f44bd2b8850b4c
IvanaMAndrade <mandrade.ivana@gmail.com>
2026-04-12 16:38:46 +0200

    Running_Version

--- Frontend/HouseholdTaskOverviewAngular/src/app/services/housemate-service.ts
+++ Frontend/HouseholdTaskOverviewAngular/src/app/services/housemate-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Housemate } from '../model/housemate';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({ providedIn: 'root' })
 export class HousemateService {
