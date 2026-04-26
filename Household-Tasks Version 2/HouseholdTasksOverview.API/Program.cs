86f7fc4bbe1bca40b96222fdee166f86d23cea2b
Lexi22B <alex_Lulu7@yahoo.com>
2026-04-26 15:50:53 +0200

    fixed API program.cs

--- Household-Tasks Version 2/HouseholdTasksOverview.API/Program.cs
+++ Household-Tasks Version 2/HouseholdTasksOverview.API/Program.cs
@@ -3,8 +3,18 @@ using HouseholdTasksOverview.Model.Repository.cs;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,19 +24,20 @@ builder.Services.AddScoped<HousematesRepository, HousematesRepository>();
 builder.Services.AddScoped<RoomsRepository, RoomsRepository>();
 builder.Services.AddScoped<TaskAssignmentsRepository, TaskAssignmentsRepository>();
 builder.Services.AddScoped<TaskCompletionsRepository, TaskCompletionsRepository>();
-builder.Services.AddScoped<TasksRepository, TasksRepository>(); //dependency injection
-
+builder.Services.AddScoped<TasksRepository, TasksRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(); //test on browser
+    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 
