833fb58b2736388bd74fac3c4fb9dc79c4b842d1
Siri0-0 <siri.general@pm.me>
2026-04-30 22:01:41 +0200

    Update the home and create system
    The create buttom now work

--- Frontend/HouseholdTaskOverviewAngular/src/app/component/home/home.ts
+++ Frontend/HouseholdTaskOverviewAngular/src/app/component/home/home.ts
@@ -1,19 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { RoomService } from '../../services/room-service';
+import { Room } from '../../model/room';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  rooms: Room[] = [];
+  showPopup = false;
 
+  newRoom = {
+    name: '',
+    selectedImage: 'assets/rooms/room1.png'
+  };
 
-  constructor(private router: Router) {}
+  roomImageOptions = [
+    'assets/rooms/room1.png',
+    'assets/rooms/room2.png',
+    'assets/rooms/room3.png'
+  ];
+
+  constructor(private router: Router, private roomService: RoomService) {}
+
+  ngOnInit() {
+    this.loadRooms();
+  }
+
+  loadRooms() {
+    this.roomService.getAll().subscribe(rooms => {
+      this.rooms = rooms.filter(r => r.householdId === 1);
+    });
+  }
 
   goToHouseProfile() {
     this.router.navigate(['/house-profile']);
@@ -23,25 +48,35 @@ export class HomeComponent {
     this.router.navigate(['/roommates']);
   }
 
-  addRoom() {
-    this.router.navigate(['/create-room']);
+  openPopup() {
+    this.newRoom = { name: '', selectedImage: 'assets/rooms/room1.png' };
+    this.showPopup = true;
   }
 
- //rooms: any[] = [];
+  closePopup() {
+    this.showPopup = false;
+  }
 
-rooms = [
-  { id: 1, name: 'Kitchen', image: 'assets/rooms/default.png' }
-];
+  createRoom() {
+    if (!this.newRoom.name) return;
 
-/*openRoom(id: number) {
-  this.router.navigate(['/room', id], {
-    state: { roomName: 'Room', roomImage: 'assets/rooms/default.png' }
-  });
-}*/
+    const room: Room = {
+      id: 0,
+      householdId: 1,
+      roomName: this.newRoom.name
+    };
 
-openRoom(room: any) {
-  this.router.navigate(['/room', room.id], {
-    state: { roomName: room.name, roomImage: room.image }
-  });
-}
+    this.roomService.create(room).subscribe((newId: number) => {
+      this.closePopup();
+      this.router.navigate(['/room', newId], {
+        state: { roomName: this.newRoom.name }
+      });
+    });
+  }
+
+  openRoom(room: Room) {
+    this.router.navigate(['/room', room.id], {
+      state: { roomName: room.roomName }
+    });
+  }
 }
