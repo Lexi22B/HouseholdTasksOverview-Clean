d4e4c8a793bfc5618889cdba347da58a8f9c8cb2
Siri0-0 <siri.general@pm.me>
2026-04-30 20:31:50 +0200

    Delete list and card components

--- Frontend/HouseholdTaskOverviewAngular/src/app/app.routes.ts
+++ Frontend/HouseholdTaskOverviewAngular/src/app/app.routes.ts
@@ -3,12 +3,6 @@
 import { Routes } from '@angular/router';
 
 //Not implemented
-import { HouseholdList } from './component/household-list/household-list'; 
-import { HousemateList } from './component/housemate-list/housemate-list';
-import { RoomList } from './component/room-list/room-list';
-import { TaskList } from './component/task-list/task-list';
-import { TaskAssignmentList } from './component/task-assignment-list/task-assignment-list';
-import { TaskCompletionList } from './component/task-completion-list/task-completion-list';
 
 //Implemented pages
 import { WelcomeComponent } from './component/welcome/welcome';
@@ -17,21 +11,15 @@ import { HomeComponent } from './component/home/home';
 import { RoomViewComponent } from './component/room-view/room-view';
 import { CreateTaskComponent } from './component/create-task/create-task';
 
-export const routes: Routes = 
-[
-    { path: 'households', component: HouseholdList },
-    { path: 'housemates', component: HousemateList },
-    { path: 'rooms', component: RoomList },
-    { path: 'tasks', component: TaskList },
-    { path: 'taskassignments', component: TaskAssignmentList },
-    { path: 'taskcompletions', component: TaskCompletionList },
-    { path: '', component: WelcomeComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'home', component: HomeComponent },
-    //{ path: 'house-profile', component: ... },  // later
-    //{ path: 'roommates', component: ... },       // later
-    //{ path: 'create-room', component: ... },     // later
-    { path: 'room/:id', component: RoomViewComponent },
-    { path: 'create-task', component: CreateTaskComponent },   
+export const routes: Routes =
+    [
+        { path: '', component: WelcomeComponent },
+        { path: 'register', component: RegisterComponent },
+        { path: 'home', component: HomeComponent },
+        //{ path: 'house-profile', component: ... },  // later
+        //{ path: 'roommates', component: ... },       // later
+        //{ path: 'create-room', component: ... },     // later
+        { path: 'room/:id', component: RoomViewComponent },
+        { path: 'create-task', component: CreateTaskComponent },
 
-];
+    ];
